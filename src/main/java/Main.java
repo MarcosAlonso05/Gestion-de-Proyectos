5fac0cd39851795bcbd46377fc30d0495b09f5c1
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-03-16 21:23:56 +0100

    Implementacion de fragmento de codigo patron Adapter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 
 // Ejemplo de uso
 
+import IA_ServicioExterno.Analisis_IA;
+import IA_ServicioExterno.DelayPredicator;
+import IA_ServicioExterno.DelayPredicatorAdapter;
 import factory.ProjectFactory;
 import projects.Project;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Patron Factory Method
         ProjectFactory factory = new ProjectFactory();
 
         Project smallProject = factory.createProject("small");
@@ -13,5 +18,12 @@ public class Main {
 
         Project bigProject = factory.createProject("big");
         bigProject.execute();
+
+        //Patron Adapter
+        Analisis_IA externalService = new Analisis_IA();
+        DelayPredicator adapter = new DelayPredicatorAdapter(externalService);
+
+        String prediction = adapter.predict("5");
+        System.out.println(prediction); // Retraso predicho: 5 días
     }
 }
